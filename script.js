f3f5aeaebf0386cc79cc1e8ebbdbab917cbe346c
Chue Chang <chuechang1996@outlook.com>
2021-12-24 17:09:22 -0600

    add id to button

--- script.js
+++ script.js
@@ -11,26 +11,35 @@ for (var i = 0; i < 9; i++){
     sectionEl.attr('class','row');
     timeblockEl = $('<label>'+ (i+9) + '<br>'+ ("am") + '</label>');
     timeblockEl.attr('name',9 + i + "am")
+    textAreaEl = $('<textarea>');
+    textAreaEl.attr('data-index', i);
+    textAreaEl.attr('id', 9 + i +"am")
         if (i > 2){
             timeblockEl.attr('name', 9 + i + "pm");
+            textAreaEl.attr('id', 9 + i +"pm")
             // this code will change the loop from writing am to pm instead.
             timeblockEl = $('<label>'+ (i+9) + '<br>'+ ("pm") + '</label>');
             if (i > 3){
                 timeblockEl.attr('name', i - 3 + "pm");
+                textAreaEl.attr('id', i-3 +"pm")
                 timeblockEl = $('<label>'+ (i-3) + '<br>'+ ("pm") + '</label>');
                 // this code will change the once it pass 12pm. It is reset back to 1.
             }
         }
     timeblockEl.attr('class', 'hour');
-    textAreaEl = $('<textarea>');
-    textAreaEl.attr('data-index', i);
     textAreaEl.attr('class', 'future');
     textAreaEl.attr('cols', '80%');
     saveBtnEl = $('<button>SAVE</button>');
     saveBtnEl.attr('class', 'saveBtn');
+    saveBtnEl.attr('data-index', i)
     containerEl.append(sectionEl);
     sectionEl.append(timeblockEl);
     sectionEl.append(textAreaEl);
     sectionEl.append(saveBtnEl);
+    savingEl = saveBtnEl.attr('data-index', i)
+    saveBtnEl = $("9am").on("click", function(){
+        console.log($("9am").value)
+    })
+    // all the code here is to add element and attribute to each tag. 
 }
 
